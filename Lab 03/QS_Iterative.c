2d043373a5c0e52c4198fe0afa441bd69a6109a2
pmosharrof21 <pmosharrof3103@gmail.com>
2025-07-20 18:16:27 +0600

    Add files via upload

---
+++ Lab 03/QS_Iterative.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
+}
+
+void quickSortIterative(int arr[], int low, int high) {
+    int stack[high - low + 1];
+    int top = -1;
+
+    stack[++top] = low;
+    stack[++top] = high;
+
+    while (top >= 0) {
+
+        high = stack[top--];
+        low = stack[top--];
+
+
+        int pi = partition(arr, low, high);
+
+
+        if (pi - 1 > low) {
+            stack[++top] = low;
+            stack[++top] = pi - 1;
+        }
+
+        if (pi + 1 < high) {
+            stack[++top] = pi + 1;
+            stack[++top] = high;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {29, 10, 14, 37, 13, 5, 21};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSortIterative(arr, 0, n - 1);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
+
