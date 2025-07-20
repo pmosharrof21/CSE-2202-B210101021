e23746da5c73bd4497c16b0d06c8cdb3a32153f4
pmosharrof21 <pmosharrof3103@gmail.com>
2025-07-20 17:37:12 +0600

    Add files via upload

---
+++ Lab 01/BS_Step.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int binarySearch(int arr[], int size, int target) {
+    int low = 0, high = size - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12, 14};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 8;
+    int result = binarySearch(arr, size, target);
+    if (result != -1)
+        printf("Element found at index %d\n", result);
+    else
+        printf("Element not found.\n");
+    return 0;
+}
+
